5d61685ca1d7789e8ac946b706bf02fddcb6f55f
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-11-25 11:57:43 -0500

    Boilerplate app structure for Quiz App project

---
+++ app.js
@@ -0,0 +1,43 @@
+/**
+ * Example store structure
+ */
+const store = {
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'What color is broccoli?',
+      answers: [
+        'red',
+        'orange',
+        'pink',
+        'green'
+      ],
+      correctAnswer: 'green'
+    },
+    {
+      question: 'What is the current year?',
+      answers: [
+        '1970',
+        '2015',
+        '2019',
+        '2005'
+      ],
+      correctAnswer: '2019'
+    }
+  ],
+  questionNumber: 0,
+  score: 0
+};
+
+/**
+ *
+ * Your app should include a render() function, that regenerates
+ * the view each time the store is updated. See your course
+ * material, consult your instructor, and reference the slides
+ * for more details.
+ *
+ * NO additional HTML elements should be added to the index.html file.
+ *
+ * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
+ *
+ */
