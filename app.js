a4b01a48e12fbefa50b1037e8ff63e1e06ce0132
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2020-02-13 14:09:52 -0500

    Adding additional comments to JS

--- app.js
+++ app.js
@@ -31,14 +31,28 @@ const store = {
 };
 
 /**
- *
- * Your app should include a render() function, that regenerates
- * the view each time the store is updated. See your course
- * material, consult your instructor, and reference the slides
- * for more details.
+ * 
+ * Technical requirements:
+ * 
+ * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ * See your course material, consult your instructor, and reference the slides for more details.
  *
  * NO additional HTML elements should be added to the index.html file.
  *
  * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
  *
+ * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
+ * 
  */
+
+/********** TEMPLATE GENERATION FUNCTIONS **********/
+
+// These functions return HTML
+
+/********** RENDER FUNCTION(S) **********/
+
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
+
+/********** EVENT HANDLER FUNCTIONS **********/
+
+// These functions handle events (submit, click, etc)
