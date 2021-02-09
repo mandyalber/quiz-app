7f001d12e090efb50cbdc160ad7ea6aac6a7410c
Amanda (Mandy) Alber <mandy.alber@gmail.com>
2021-02-09 12:30:32 -0800

    clean up console.log()s

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 // These functions return HTML templates
 
 //show the start screen
-function displayStartTemplate(){
-  console.log('displayStartTemplate function ran')  
+function displayStartTemplate(){ 
   return `<div class="welcome-page">    
             <p>Welcome to my quiz on baby sleep. Find out how much you know about how babies sleep.</p>
             <button class="button" type="button" id="start">Start Quiz</button> 
@@ -12,8 +11,7 @@ function displayStartTemplate(){
 }
 
 //show users which question they're on and their current score
-function displayProgressTemplate() {
-  console.log('displayProgressTemplate function ran')  
+function displayProgressTemplate() { 
   return `<ul class="progress-bar">
             <li class="question-number">Question ${store.questionNumber + 1} of ${store.questions.length}.</li>
             <li class="score">Answered ${store.score} correct out of ${store.questions.length}.</li>
@@ -22,7 +20,6 @@ function displayProgressTemplate() {
 
 //display the answers as list items with radio buttons
 function displayAnswersTemplate(){
-  console.log('displayAnswersTemplate function ran')
   const answers = store.questions[store.questionNumber].answers
   let answersDisplay = ''
   for (let i=0;i<answers.length;i++){
@@ -36,7 +33,6 @@ function displayAnswersTemplate(){
 
 //display the question and answer choices
 function displayQuestionTemplate(){
-  console.log('displayQuestionTemplate function ran')  
   const currentQuestion = store.questions[store.questionNumber]
   return `<form id="quiz-question">
             <p class="question">${currentQuestion.question}</p>
@@ -47,7 +43,6 @@ function displayQuestionTemplate(){
 
 //tells user if they were correct or if they were incorrect/correct answer
 function displayFeedbackTemplate(answer) {
-  console.log('displayFeedbackTemplate function ran')
   let correctAnswer = store.questions[store.questionNumber].correctAnswer
   let feedback = ''
 
@@ -61,8 +56,7 @@ function displayFeedbackTemplate(answer) {
 }
 
 //show users their overall score at the end of the quiz
-function displayResultsTemplate() {
-  console.log('displayResultsTemplate function ran')  
+function displayResultsTemplate() { 
   return `<div class="results-page">  
             <h2>Results</h2>
             <p class="results-text">Your final score was ${store.score} correct out of ${store.questions.length}.</p><p>Thanks for taking my quiz. If you would like to try again, please click the button below:</p>
@@ -75,7 +69,6 @@ function displayResultsTemplate() {
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
 
 function renderQuiz(){
-  console.log('renderQuiz function ran')
   let html = '';
 
   if (store.quizStarted === false) {
@@ -99,27 +92,22 @@ function renderQuiz(){
 
 //handles users clicking button to start the quiz.
 function handleStartClick() {
-  console.log('handleStartClick function ran')  
   $('main').on('click', '#start', function(event){
-    store.quizStarted = true
-    console.log(store)    
+    store.quizStarted = true  
     renderQuiz()
   })
 }
 
 //handles users submitting their answer and checks if it is correct
 function handleSubmitClicked(){
-  console.log('handleSubmitClicked function ran')
   $('body').on('submit', '#quiz-question', function(event){
     event.preventDefault()
     //variable for current question
     const currentQuestion = store.questions[store.questionNumber]
     //variable for selection
     let selection = $('input[name=answers]:checked').val()
-    console.log(selection)
     //check if selection is equal to current question's correct answer
     //if yes, increment score and render feedback page with correct text
-    console.log(currentQuestion.correctAnswer)
     if (selection === currentQuestion.correctAnswer){
       store.score++
       $('#quiz-question').append(displayFeedbackTemplate('correct'))
@@ -141,7 +129,6 @@ function handleSubmitClicked(){
 
 //handles users clicking next button
 function handleNextClicked(){
-  console.log('handleNextClicked function ran')
   $('body').on('click', '#next', function(event){
     renderQuiz()
   })
@@ -149,7 +136,6 @@ function handleNextClicked(){
 
 //handles users clicking button to start a new quiz
 function handleRestartClicked(){
-  console.log('handleRestartClicked function ran') 
   $('main').on('click','#restart', function(event){
     store.quizStarted = false
     store.questionNumber = 0
